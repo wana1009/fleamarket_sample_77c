87aa5fb8c8e273e8a0a86ad7b821d677cb2167ea
kazukiogushi <asahisoccer71@gmail.com>
2020-08-26 21:44:29 +0900

    コメントアウトを元に戻す

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :condition
-  # belongs_to_active_hash :charge
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :day
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
 
   belongs_to :user
   belongs_to :category
@@ -13,8 +13,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :detail, presence: true
   validates :price, presence: true
-  # validates :condition, presence: true
-  # validates :charge, presence: true
-  # validates :prefecture, presence: true
-  # validates :days, presence: true
+  validates :condition, presence: true
+  validates :charge, presence: true
+  validates :prefecture, presence: true
+  validates :days, presence: true
 end
