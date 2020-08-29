4b388f9321b6284300651a39ad768625670fd4b2
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-08-29 16:55:11 +0900

    wizard_address

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  root to: 'items#index'
 end
