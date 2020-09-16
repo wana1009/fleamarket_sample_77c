a2d81047478c4bc2a55c0fd41a71893c3d4d979b
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-09-16 14:38:07 +0900

    Revert "Revert "Merge pull request #21 from wana1009/user-login-front-end""
    This reverts commit 010250c4da1b342d606f6f4b2d112a4c5b9a7a24.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show]
   def index
     @items = Item.all.includes(:images)
   end
-
-  def show
-    @seller = @item.seller
-    @user = @seller.user
-    @category = @item.category
-    @brand = @item.brand
-    @images = @item.images
-  end
-
-  private
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
 end
