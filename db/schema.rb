6eca99ace6d9d1172339a24a9477cbc0d22e4e52
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-09-27 17:20:20 +0900

    登録画面微調整

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_045923) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_045923) do
     t.integer "day_id", null: false
     t.integer "seller_id", null: false
     t.integer "order_id"
-    t.integer "category_id", null: false
+    t.integer "category_id"
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
