370bbacbf3a770e5cb6d1d0726b9c45752d75e8c
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-09-29 10:58:16 +0900

    エラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :day
 
-  #belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :category, optional: true
   belongs_to :brand, optional: true
   has_many :images
