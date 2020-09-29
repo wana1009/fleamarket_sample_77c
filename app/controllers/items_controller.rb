71b040a4658ebafccb6585966a4ed8f3d1af7015
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-09-29 11:31:18 +0900

    削除ボタン設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :destroy]
   def index
     @items = Item.all.includes(:images)
   end
@@ -17,6 +17,16 @@ class ItemsController < ApplicationController
     item.destroy
   end
 
+
+  def destroy
+    if current_user.id == @item.user_id && @item.destroy
+      render template: "items/destroy"
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  
   private
   def set_item
     @item = Item.find(params[:id])
