176300e0bb59527b1b5d87f86fac220298940ee1
wana1009 <wanaryo1009@gmail.com>
2020-09-30 17:40:07 +0900

    form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
   
   def create
-    # binding.pry
+    binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
