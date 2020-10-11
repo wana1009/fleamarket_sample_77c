6cc8a95bf80d649927395805edbe63ad3b7d20b3
wana1009 <wanaryo1009@gmail.com>
2020-10-11 21:32:07 +0900

    activestorage delete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
+// = require activestorage
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
