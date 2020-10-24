2d702ee5436053698ac64055ac76d3430b76bd7a
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-10-24 17:40:30 +0900

    購入画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  require "payjp"
 
   def index
     @order = Order.new
@@ -11,6 +12,57 @@ class OrdersController < ApplicationController
       return redirect_to root_path
     else
       render 'index'
+    end
+  end
+
+  def buy
+    @item = Item.find(params[:item_id])
+    @images = @item.images.all
+
+    if user_signed_in?
+      @user = current_user
+      if @user.card.present?
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        @card = Card.find_by(user_id: current_user.id)
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        @customer_card = customer.cards.retrieve(@card.card_id)
+
+        @exp_month = @customer_card.exp_month.to_s
+        @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+      else
+      end
+    else
+      redirect_to user_session_path, alert: "ログインしてください"
+    end
+  end
+
+  def pay
+    @item = item.find(params[:item_id])
+    @images = @item.images.all
+
+    if @item.orders.present?
+      redirect_to item_path(@item.id), alert: "売り切れています。"
+    else
+      # 二重で購入処理を防ぐ
+      @item.with_lock do
+        if current_user.card.present?
+          # ログインユーザーがクレジットカード登録済みの場合
+          @card = Card.find_by(user_id: current_user.id)
+          Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+          charge = Payjp::Charge.create(
+          amount: @item.price,
+          customer: Payjp::Customer.retrieve(@card.customer_id),
+          currency: 'jpy'
+          )
+        else
+          Payjp::Charge.create(
+          amount: @item.price,
+          card: params['payjp-token'],
+          currency: 'jpy'
+          )
+        end
+      @order = Order.create(seller_id: current_user.id, item_id: params[:item_id])
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,18 @@ Rails.application.routes.draw do
       get 'get_category_grandchild', to: 'items#get_category_grandchild'
     end
 
-    resources :buyers, only: [:index] do
-      collection do
-        get 'done', to: 'buyers#done'
-        post 'pay', to: 'buyers#pay'
+  #   # resources :buyers, only: [:index] do
+  #     collection do
+  #       get 'done', to: 'buyers#done'
+  #       post 'pay', to: 'buyers#pay'
+  #     end
+  #   end
+  # end
+
+    resources :orders, only: [:index, :create] do
+      member do
+        get  "buy"
+        post "pay"
       end
     end
   end
@@ -39,4 +47,5 @@ Rails.application.routes.draw do
       get  'confirmation'
     end
   end
+
 end
