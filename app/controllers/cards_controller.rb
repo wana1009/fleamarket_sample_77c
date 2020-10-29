b14adb826445033998fa2da50875ff1d55d2f5c8
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-10-29 17:50:12 +0900

    差分変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,11 @@
 class CardsController < ApplicationController
   require "payjp"
 
+  def index
+    @card = Card.find_by(user_id: current_user.id)
+  end
+
+
   def new
     @card = Card.where(user_id: current_user.id)
     redirect_to card_path(current_user.id) if @card.exists?
