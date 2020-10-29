3409ed38b3b85b06f4014c2d68fcff3295f515c1
Account-aiZawa <hl7q.sl77.o6o@icloud.com>
2020-10-30 05:15:34 +0900

    item_edit2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :show_all_instance, only: [:edit, :update]
+  before_action :set_category_array, only: [:edit, :update]
   def index
     @items = Item.all.includes(:images)
   end
@@ -39,7 +41,7 @@ class ItemsController < ApplicationController
     @user = @seller.user
     @category = @item.category
     @brand = @item.brand
-    @images = @item.images
+    @images = Image.where(item_id: params[:id])
     @category_grandchildren = @item.category
     @category_children = @category_grandchildren.parent
     @category_parent = @category_children.parent  
@@ -54,53 +56,67 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.seller_id
-      @category_parent_array = Category.where(ancestry: nil).each do |parent|
-      end
-
-      @user = User.find(@item.seller_id)
-      @images = Image.where(item_id: params[:id])
-      @category_id = @item.category_id
-      @category_parent = Category.find(@category_id).parent.parent
-      @category_child = Category.find(@category_id).parent
-      @category_grandchild = Category.find(@category_id)
-      grandchild = @item.category
-      child = grandchild.parent
-
-      @parent_array = []
-      @parent_array << @item.category.parent.parent.name
-      @parent_array << @item.category.parent.parent.id
-
-      @category_children_array = Category.where(ancestry: child.ancestry)
-      @child_array = []
-      @child_array << child.name
-      @child_array << child.id
-
-      @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
-      @grandchild_array = []
-      @grandchild_array << grandchild.name
-      @grandchild_array << grandchild.id
-
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    grandchild = @item.category
+    child = grandchild.parent
+    @parent_array = []
+    @parent_array << @item.category.parent.parent.name
+    @parent_array << @item.category.parent.parent.id
+    @category_children_array = Category.where(ancestry: child.ancestry)
+    @child_array = []
+    @child_array << child.name
+    @child_array << child.id 
+    @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
+    @grandchild_array = []
+    @grandchild_array << grandchild.name
+    @grandchild_array << grandchild.id
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to root_path
-    else
+    if item_params[:images_attributes].nil?
       render :edit
+    else
+      exit_ids = []
+      item_params[:images_attributes].each do |a,b|
+        exit_ids << item_params[:images_attributes].dig(:"#{a}",:id).to_i
+      end
+      ids = Image.where(item_id: params[:id]).map{|image| image.id }
+      delete__db = ids - exit_ids
+      Image.where(id:delete__db).destroy_all
+      @item.touch
+      if @item.update(item_params)
+        redirect_to  update_done_items_path
+      else
+        render :edit
+      end
     end
   end
 
+  def update_done
+    @item_update = Item.order("updated_at DESC").first
+  end
+
   private
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :condition_id, :charge_id, :prefecture_id, :day_id, :price, images_attributes: [:url, :_destroy, :id] ).merge(seller_id: current_user.id)
+  end
+
   def set_item
     @item = Item.find(params[:id])
   end
 
-  def item_params
-    params.require(:item).permit(:name, :detail, :category_id, :condition_id, :charge_id, :prefecture_id, :day_id, :price, images_attributes: [:url, :_destroy, :id] ).merge(seller_id: current_user.id)
+  def set_category_array
+    @category_parent_array = Category.where(ancestry: nil).each do |parent|
+    end
+  end
+
+  def show_all_instance
+    @user = User.find(@item.seller_id)
+    @images = Image.where(item_id: params[:id])
+    @images_first = Image.where(item_id: params[:id]).first
+    @category_id = @item.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,15 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
   root to: 'items#index'
 
   resources :items do
-
     collection do
       get 'search'
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
+      get 'update_done'
     end
   end
 
