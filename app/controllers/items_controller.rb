242313ba6051173681b580fbebfa0cb2a6b9561d
Ryuki-hmst <ryu55kkhar@gmail.com>
2020-10-30 11:40:37 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -63,7 +63,7 @@ class ItemsController < ApplicationController
       :card => params['payjp-token'],
       :currency => 'jpy'
     )
-    @item.update(buyer_id: current_user.id)
+    @item.update(order_id: current_user.id)
   end
     
   def edit
